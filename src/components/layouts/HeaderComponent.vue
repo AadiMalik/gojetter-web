b277c5970b7a9a7a133578828feb9e8318ee9690
Aadi Malik <adnanjunaid76279@gmail.com>
2025-08-20 05:04:30 +0500

    cart link add in header

--- src/components/layouts/HeaderComponent.vue
+++ src/components/layouts/HeaderComponent.vue
@@ -33,7 +33,7 @@
                     <li v-if="!user"><router-link to="/login" active-class="active">Login</router-link></li>
                     <li v-else class="dropdown">
                         <a href="#" style="cursor: pointer;">
-                            <i class="fa fa-person"></i> {{ user.name }} <i
+                            <span class="bi bi-person" style="font-size: 18px;"></span> {{ user.name }} <i
                                 class="bi bi-chevron-down toggle-dropdown"></i>
                         </a>
                         <ul>
@@ -41,23 +41,55 @@
                             <li><a @click.prevent="logout" style="cursor: pointer;">Logout</a></li>
                         </ul>
                     </li>
+                    <li v-if="user">
+                        <router-link to="/cart" style="padding: 5px 5px;">
+                            <i class="bi bi-cart" style="font-size: 18px;"></i> Cart
+                            <span v-if="cartCount > 0"
+                                class="badge bg-danger rounded-pill position-absolute top-0 start-100 translate-middle">
+                                {{ cartCount }}
+                            </span>
+                        </router-link>
+                    </li>
                 </ul>
                 <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
             </nav>
 
-            <router-link class="btn-getstarted" to="/destinations">Get Started</router-link>
+            <!-- <router-link class="btn-getstarted" to="/destinations">Get Started</router-link> -->
 
         </div>
     </header>
 </template>
 <script setup>
-import { useAuthStore } from '@/store/auth'
-import { computed } from 'vue'
+import { ref, onMounted, computed } from "vue"
+import { useAuthStore } from "@/store/auth"
+import api from "@/api"
 
 const auth = useAuthStore()
 const user = computed(() => auth.user)
 
+const cartCount = ref(0)
+
+async function fetchCart() {
+    if (!auth.token) return
+    try {
+        const res = await api.get("/cart-list", {
+            headers: { Authorization: `Bearer ${auth.token}` }
+        })
+        cartCount.value = res.data?.Data?.length || 0
+    } catch (err) {
+        console.error("Cart fetch error", err)
+        cartCount.value = 0
+    }
+}
+
 function logout() {
     auth.logout()
+    cartCount.value = 0
 }
+
+onMounted(() => {
+    if (auth.token) {
+        fetchCart()
+    }
+})
 </script>
